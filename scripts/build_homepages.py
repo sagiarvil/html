f9b285b52e7a56053f93da919cf542c281061e2a
Baris Bagirlar <barisbagirlar@gmail.com>
2026-09-12 15:32:25 +0300

    feat(design): deploy ultra-premium footer, macOS floating dock and bento v3

--- scripts/build_homepages.py
+++ scripts/build_homepages.py
@@ -112,9 +112,9 @@ def build_homepages():
 <link rel="icon" href="/favicon.ico?v=hh5" sizes="any">
 <link rel="manifest" href="/site.webmanifest">
 <link rel="stylesheet" href="/assets/css/enterprise-theme-system.css">
-<link rel="stylesheet" href="/assets/css/validator.css?v=11">
+<link rel="stylesheet" href="/assets/css/validator.css?v=12">
 <link rel="stylesheet" href="/assets/css/theme.css?v=12">
-<link rel="stylesheet" href="/assets/css/premium-experience.css?v=6">
+<link rel="stylesheet" href="/assets/css/premium-experience.css?v=7">
 <link rel="stylesheet" href="/assets/css/enterprise-system.css?v=1">
 <link rel="stylesheet" href="/assets/css/commercial-intent.css?v=2" data-commercial-intent-css="static">
 <meta name="author" content="HTML&amp;HTML">
@@ -851,9 +851,9 @@ export const revalidate = 3600; // Cloudflare edge cache</code></pre>
 <link rel="icon" href="/favicon.ico?v=hh5" sizes="any">
 <link rel="manifest" href="/site.webmanifest">
 <link rel="stylesheet" href="/assets/css/enterprise-theme-system.css">
-<link rel="stylesheet" href="/assets/css/validator.css?v=11">
+<link rel="stylesheet" href="/assets/css/validator.css?v=12">
 <link rel="stylesheet" href="/assets/css/theme.css?v=12">
-<link rel="stylesheet" href="/assets/css/premium-experience.css?v=6">
+<link rel="stylesheet" href="/assets/css/premium-experience.css?v=7">
 <link rel="stylesheet" href="/assets/css/enterprise-system.css?v=1">
 <link rel="stylesheet" href="/assets/css/commercial-intent.css?v=2" data-commercial-intent-css="static">
 <meta name="author" content="HTML&amp;HTML">
